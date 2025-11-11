5b8ffe236e8fd5906aa7d82b64ab2a37a530c4a5
Noury Abdourahamane <78423663+NouryAbdou@users.noreply.github.com>
2025-11-12 00:34:41 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Flexy-payments'
+copyright = '2025, Flexy-payments'
+author = 'ACITIM'
 
 release = '0.1'
 version = '0.1.0'
